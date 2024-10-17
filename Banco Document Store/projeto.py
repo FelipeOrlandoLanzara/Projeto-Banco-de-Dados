ad243dda40fc1c2a3b9e28fcbb548b445820dead
jvgoverna <105528242+jvgoverna@users.noreply.github.com>
2024-10-17 08:36:31 -0300

    Aggregate

--- Banco Document Store/projeto.py
+++ Banco Document Store/projeto.py
@@ -59,7 +59,6 @@ nome_materia = DynamicProvider(
 #inicialização dos providers:
 fake.add_provider(nome_materia)
 
-
 ra_aluno = random.sample(range(100000000, 500000000), 50)
 ra_professor = random.sample(range(500000001, 999999999), 20)
 lista_materias = [num for num in random.sample(range(100000, 999999), 60)]
@@ -78,14 +77,15 @@ for i in ra_professor:
 primary_keys = {
     "nome_departamento" : ["Matemática", "Física", "Ciência da Computação", "Engenharia Elétrica", "Engenharia Mecânica"],
     "id_curso" : ['MA', 'FI', 'CC', 'EE', 'EM'],
-    "id_materia" : ['452645', '745234', '258452', '123254', '123456', '545236'],
 }
 semestre = ["Primeiro","Segundo","Terceiro","Quarto","Quinto","Sexto","Setimo","oitavo"]
+
+
 
 materias = []
 for i in range(len(lista_materias)):
     materia = {
-        "ID_Materia" : random.choice(primary_keys["id_materia"]),
+        "ID_Materia" : lista_materias[random.randint(0, (len(lista_materias)-1))],
         "Nome_Materia" : fake.materias_random(),
         "Prova" : fake.pybool() 
     }
@@ -105,29 +105,72 @@ for i in range(len(ra_aluno)):
             "Nota" : round(random.uniform(0.0,10.0),2),
             "Semestre" : random.choice(semestre),
             "Ano" : 2020,
-            "ID_Materia" : random.choice(primary_keys["id_materia"])
+            "ID_Materia" : lista_materias[random.randint(0, (len(lista_materias)-1))]
         }
     }
     students.append(student)
 collection_student.insert_many(students)
 
-pipeline = [
+
+# Realiza o lookup entre estudantes e matérias
+resultados = collection_student.aggregate([
     {
-        "$lookup" : {
-            "from" : "students",
-            "localField" : "ID_Materia",
-            "foreignField" : "Historico_Escolar.ID_Materia",
-            "as" : "Materias_Info"
+        "$lookup": {
+            "from": "Materia",  # Nome da coleção de matérias
+            "localField": "Historico_Escolar.ID_Materia",  # Campo da coleção de estudantes
+            "foreignField": "ID_Materia",  # Campo da coleção de matérias
+            "as": "materia_info"  # Nome do campo onde a informação da matéria será armazenada
         }
     },
     {
-        "$unwind" : "$Materias_Info"
+        "$unwind": "$materia_info"  # Corrigido o nome para "materia_info"
     }
-]
+])
 
-result = db.collection_materia.aggregate(pipeline)
-print(result)
+#precisa conferir
+a=0
+b=0
 
+
+for i, resultado in enumerate(resultados):
+    print(f'{i}- {resultado}\n')
+
+ids_repetidos = []
+lista_resultado = []
+for resultado in resultados:
+    a+=1
+    #print(f'{i+1}- {resultado}\n')
+    if resultado['ID_Aluno'] not in ids_repetidos:
+        ids_repetidos.append(resultado['ID_Aluno'])
+        lista_resultado.append(resultado)
+
+for i, resultado in enumerate(lista_resultado):
+    #print(f'{i}- {resultado}\n')
+    b+=1
+    pass
+
+print(a)
+print(b)
+#aaaaaaaaaaaaaaaaaaaaaaa
+
+
+#{'_id': ObjectId('671009ddd2200dae3b2991cf'), 'ID_Aluno': '19.190.190-1', 'Nome_Aluno': 'Kyle', 'Idade_Aluno': 50, 'ID_Curso': 'EE', 'ID_TCC': 49, 'Historico_Escolar': {'ID_Historico_Escolar': 1, 'Nota': 4.13, 'Semestre': 'oitavo', 'Ano': 2020, 'ID_Materia': '745234'}, 'materia_info': {'_id': ObjectId('671009ddd2200dae3b29916b'), 'ID_Materia': '745234', 'Nome_Materia': 'Introdução a Computação', 'Prova': False}}
+#{'_id': ObjectId('671009ddd2200dae3b2991cf'), 'ID_Aluno': '19.190.190-1', 'Nome_Aluno': 'Kyle', 'Idade_Aluno': 50, 'ID_Curso': 'EE', 'ID_TCC': 49, 'Historico_Escolar': {'ID_Historico_Escolar': 1, 'Nota': 4.13, 'Semestre': 'oitavo', 'Ano': 2020, 'ID_Materia': '745234'}, 'materia_info': {'_id': ObjectId('671009ddd2200dae3b299175'), 'ID_Materia': '745234', 'Nome_Materia': 'Calculo 1', 'Prova': False}}
+
+
+
+
+
+# Exibir os resultados
+#print(resultados)
+#resultados_lista = list(resultados)
+#print(resultados_lista)
+# if resultados:
+#     print("\nResultados da agregação:")
+#     for resultado in db["materia_info"]:
+#         print(resultado)
+# else:
+#     print("Nenhum resultado encontrado na agregação.")
 
 teachers = []
 for i in range(len(ra_professor)):
@@ -140,7 +183,7 @@ for i in range(len(ra_professor)):
             "Semestre" : random.choice(semestre),
             "Ano" : 2020,
             "Quantidade_Aulas" : random.randint(1, 100),
-            "ID_Materia" : random.choice(primary_keys["id_materia"]) 
+            "ID_Materia" : lista_materias[random.randint(0, (len(lista_materias)-1))] 
         }
     }
     teachers.append(teacher)
@@ -170,8 +213,8 @@ collection_teacher.insert_many(teachers)
 
 #QUERIES
 # 1- histórico escolar de qualquer aluno, retornando o código e nome da disciplina, semestre e ano que a disciplina foi cursada e nota final
-#hist_esc = collection_student.find_one({"Nome_Aluno":0, "_id":1})
-
+#hist_esc = lista_resultado.find_one({}, {"ID_Materia":1, "Nome_Materia":1, "Historico_Escolar.Semestre":1, "Historico_Escolar.Ano":1, "Historico_Escolar.Nota":1})
+#print(hist_esc)
 
 # 2- histórico de disciplinas ministradas por qualquer professor, com semestre e ano
 hist_prof = collection_teacher.find_one({}, {"Nome_Professor":1, "Historico_Professor.ID_Materia":1, "Historico_Professor.Semestre":1, "Historico_Professor.Ano":1})
@@ -187,4 +230,5 @@ print(hist_prof)
 
 
 # 5- saber quais alunos formaram um grupo de TCC e qual professor foi o orientador
+
 

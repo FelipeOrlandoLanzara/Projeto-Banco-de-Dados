4528a41183d4b772673fbee7a2828ad63f57ed1b
jvgoverna <105528242+jvgoverna@users.noreply.github.com>
2024-11-06 14:22:02 -0300

    Finalização banco Neo4j

--- GraphDatabase/projeto.py
+++ GraphDatabase/projeto.py
@@ -62,6 +62,8 @@ def removeNos():
     with driver.session() as session:
         session.run("MATCH (n) DETACH DELETE n")
 
+# ----- CRIAÇÃO DE TODAS OS GRAFOS -----
+
 # Criação da Tabela Alunos
 def criaAluno():
     with driver.session() as session:
@@ -102,6 +104,7 @@ def criaProfessor():
                         Nome_Professor = fake.first_name(),
                         Salario = random.randint(2000, 20000)
                         )
+            
 # Adiciona uma nova Professora Julia
 def adicionaProfessor():
     with driver.session() as session:
@@ -140,6 +143,7 @@ def criaDepartamento():
                         Nome_Departamento = name,
                         Chefe_Departamento = fake.first_name()
                         )
+            
 # Adiciona uma Chefe de Departamento Julia em um novo Departamento Engenharia de Produção
 def adicionaDepartamento():
     with driver.session() as session:
@@ -148,96 +152,60 @@ def adicionaDepartamento():
                     Chefe_Departamento = "Julia"
                     )
 
-
-# Chama as funções 
-try:
-    removeNos()
-    criaAluno()
-    criaHistoricoEscolar()
-    criaMateria()
-    criaProfessor()
-    adicionaProfessor()
-    criaHistoricoProfessor()
-    criaCurso()
-    criaDepartamento()
-    adicionaDepartamento()
-finally:
-    driver.close()
-    
- 
-# QUERIES
-print('----- RESOLUÇÃO DAS QUERIES -----\n')
-
-# 1- Histórico escolar de qualquer aluno, retornando o código e nome da disciplina, semestre e ano que a disciplina foi cursada e nota final
-print("----- QUERY 1 -----")
-def relacoesQuery1():
+# Criação da Tabela TCC
+def criaTCC():
     with driver.session() as session:
-        for ra, id_he, id_ma in zip(ra_aluno_formatado, lista_hist_escolar, lista_materias):
+        for id in range(len(ra_aluno_formatado)):
+            session.run("CREATE (t:TCC {ID_TCC: $ID_TCC, Titulo: $Titulo})",
+                        ID_TCC = id,
+                        Titulo = "Título: " + str(id)
+                        )
+
+
+# ----- CRIAÇÃO DE TODAS AS RELAÇÕES -----
+
+# Criação da Relação Aluno com Histórico Escolar
+def relacao_Aluno_HistoricoEscolar():
+    with driver.session() as session:
+        for ra, id_he in zip(ra_aluno_formatado, lista_hist_escolar):
             session.run(
                 "MATCH (a:Aluno {ID_Aluno: $ID_Aluno}), (he:HistóricoEscolar {ID_HistoricoEscolar: $ID_HistoricoEscolar})"
                 "CREATE (a)-[:TEM]->(he)",
                 ID_Aluno = ra, ID_HistoricoEscolar = id_he
             )
+
+# Criação da Relação Histórico Escolar com Matéria
+def relacao_HistoricoEscolar_Materia():
+    with driver.session() as session:
+        for id_he, id_ma in zip(lista_hist_escolar, lista_materias):
             session.run(
                 "MATCH (he:HistóricoEscolar {ID_HistoricoEscolar: $ID_HistoricoEscolar}), (m:Matéria {ID_Materia: $ID_Materia})"
                 "CREATE (he)-[:REFERENTE]->(m)",
                 ID_HistoricoEscolar = id_he, ID_Materia = id_ma 
             )
-relacoesQuery1()
 
-with driver.session() as session:
-    query1 = session.run(
-        """
-        MATCH (a:Aluno)-[:TEM]->(he:HistóricoEscolar)-[:REFERENTE]->(m:Matéria)
-        RETURN a.Nome_Aluno AS Nome_Aluno, 
-               m.ID_Materia AS ID_Materia, 
-               m.Nome_Materia AS Nome_Materia, 
-               he.Semestre AS Semestre, 
-               he.Ano AS Ano, 
-               he.Nota AS Nota
-        LIMIT 1
-        """
-    )
-    for resultado in query1:
-        print(f'Nome do Aluno: {resultado["Nome_Aluno"]}, ID da Matéria: {resultado["ID_Materia"]}, Nome da Matéria: {resultado["Nome_Materia"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}, Nota: {resultado["Nota"]}')
-
-
-# 2- Histórico de disciplinas ministradas por qualquer professor, com semestre e ano
-print("\n----- QUERY 2 -----")
-def relacoesQuery2():
+# Criação da Relação Professor com Histórico Professor
+def relacao_Professor_HistoricoProfessor():
     with driver.session() as session:
-        for ra, id_hp, id_ma in zip(ra_professor_formatado, lista_hist_professor, lista_materias):
+        for ra, id_hp in zip(ra_professor_formatado, lista_hist_professor):
             session.run(
                 "MATCH (p:Professor {ID_Professor: $ID_Professor}), (hp:HistóricoProfessor {ID_HistoricoProfessor: $ID_HistoricoProfessor})"
                 "CREATE (p)-[:POSSUI]->(hp)",
                 ID_Professor = ra, ID_HistoricoProfessor = id_hp
             )
+            
+# Criação da Relação Histórico Professor com Matéria
+def relacao_HistoricoProfessor_Materias():
+    with driver.session() as session:
+        for id_hp, id_ma in zip(lista_hist_professor, lista_materias):
             session.run(
-                "MATCH (hp:HistóricoProfessor {ID_HistoricoProfessor: $ID_HistoricoProfessor}), (m:Matéria {ID_Materia: $ID_Materia})"
-                "CREATE (hp)-[:REFERENCIA]->(m)",
-                ID_HistoricoProfessor = id_hp, ID_Materia = id_ma 
-            )
-relacoesQuery2()
+                        "MATCH (hp:HistóricoProfessor {ID_HistoricoProfessor: $ID_HistoricoProfessor}), (m:Matéria {ID_Materia: $ID_Materia})"
+                        "CREATE (hp)-[:REFERENCIA]->(m)",
+                        ID_HistoricoProfessor = id_hp, ID_Materia = id_ma 
+                    )
 
-with driver.session() as session:
-    query2 = session.run(
-    """
-    MATCH (p:Professor)-[:POSSUI]->(hp:HistóricoProfessor)-[:REFERENCIA]->(m:Matéria)
-    RETURN p.Nome_Professor AS Nome_Professor,
-           m.Nome_Materia AS Nome_Materia,
-           m.ID_Materia AS ID_Materia,
-           hp.Semestre AS Semestre,
-           hp.Ano AS Ano
-    LIMIT 1
-    """
-    )
-    for resultado in query2:
-        print(f'Nome do Professor: {resultado["Nome_Professor"]}, Nome da Matéria: {resultado["Nome_Materia"]}, ID da Matéria: {resultado["ID_Materia"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}')
-
-
-# 3- Listar alunos que já se formaram (foram aprovados em todos os cursos de uma matriz curricular) em um determinado semestre de um ano
-print("\n----- QUERY 3 -----")
-def relacoesQuery3():
+# Criação da Relação Aluno com Curso
+def relacao_Aluno_Curso():
     with driver.session() as session:
         for ra in ra_aluno_formatado:
             id = random.choice(primary_keys["id_curso"]) # Vai pegar um aleatório, o que permite com que alunos cursem o mesmo curso
@@ -248,30 +216,9 @@ def relacoesQuery3():
                 """,
                 ID_Aluno = ra, ID_Curso = id
             )
-relacoesQuery3()
 
-with driver.session() as session:
-    numero_aleatorio = random.randint(0, 7) # Pega um número aleatório para buscar na lista Semestre
-    semestre_aleatorio = semestre[numero_aleatorio] # Para pegar essa variável dentro da Query é preciso colocar o '$' antes
-    query3 = session.run(
-    """
-    MATCH (a:Aluno)-[:TEM]->(he:HistóricoEscolar), (a:Aluno)-[:CURSOU]->(c:Curso)
-    WHERE he.Nota > 5 AND he.Semestre = $semestre_aleatorio AND he.Ano = 2020
-    RETURN a.Nome_Aluno AS Nome_Aluno,
-           c.Nome_Curso AS Nome_Curso,
-           c.ID_Curso AS ID_Curso,
-           he.Semestre AS Semestre,
-           he.Ano AS Ano,
-           he.Nota AS Nota
-    """, 
-    semestre_aleatorio = semestre_aleatorio # E agora eu digo para a Query qual o parâmetro que eu dei
-    )
-    for resultado in query3:
-        print(f'Nome do Aluno: {resultado["Nome_Aluno"]}, Nome do Curso: {resultado["Nome_Curso"]}, ID do Curso: {resultado["ID_Curso"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}, Nota: {resultado["Nota"]}')
-
-# 4- Listar todos os professores que são chefes de departamento, junto com o nome do departamento
-print("\n----- QUERY 4 -----")
-def relacoesQuery4():
+# Criação da Relação Departamento com Professor
+def relacao_Departamento_Professor():
     with driver.session() as session:
         for ra in ra_professor_formatado:
             name = random.choice(primary_keys["nome_departamento"])
@@ -289,18 +236,166 @@ def relacoesQuery4():
             """,
             Nome_Departamento = "Engenharia de Produção", ID_Professor = "24.122.055-7"
         )
-relacoesQuery4()
 
-with driver.session() as session:
-    query4 = session.run(
-    """
-    MATCH (d:Departamento)-[:ORGANIZA]->(p:Professor)
-    WHERE d.Chefe_Departamento = p.Nome_Professor
-    RETURN d.Nome_Departamento AS Nome_Departamento,
-           p.Nome_Professor AS Nome_Professor
-    """
-    )
-    for resultado in query4:
-        print(f'Nome do Departamento: {resultado["Nome_Departamento"]}, Nome do Professor: {resultado["Nome_Professor"]}')
+# Criação da Relação Professor com TCC
+def relacao_Professor_TCC():
+    with driver.session() as session:
+        for id_tcc in range(len(ra_aluno_formatado)):
+            ra = random.choice(ra_professor_formatado)
+            session.run(
+                """
+                MATCH (p:Professor {ID_Professor: $ID_Professor}), (t:TCC {ID_TCC: $ID_TCC})
+                CREATE (p)-[:ORIENTADO]->(t)
+                """,
+            ID_TCC = id_tcc, ID_Professor = ra
+            )
+
+# Criação da Relação Aluno com TCC
+def relacao_Aluno_TCC():
+    with driver.session() as session:
+        for id_tcc, ra in enumerate(ra_aluno_formatado):
+                session.run(
+                    """
+                    MATCH (a:Aluno {ID_Aluno: $ID_Aluno}), (t:TCC {ID_TCC: $ID_TCC})
+                    CREATE (a)-[:FAZ]->(t)
+                    """,
+                ID_Aluno = ra, ID_TCC = id_tcc
+                )
+
+ 
+# ----- RESOLUÇÃO DAS QUERIES -----
+
+# 1- Histórico escolar de qualquer aluno, retornando o código e nome da disciplina, semestre e ano que a disciplina foi cursada e nota final
+def query1():
+    with driver.session() as session:
+        query1 = session.run(
+            """
+            MATCH (a:Aluno)-[:TEM]->(he:HistóricoEscolar)-[:REFERENTE]->(m:Matéria)
+            RETURN a.Nome_Aluno AS Nome_Aluno, 
+                m.ID_Materia AS ID_Materia, 
+                m.Nome_Materia AS Nome_Materia, 
+                he.Semestre AS Semestre, 
+                he.Ano AS Ano, 
+                he.Nota AS Nota
+            LIMIT 1
+            """
+        )
+        for resultado in query1:
+            print(f'Nome do Aluno: {resultado["Nome_Aluno"]}, ID da Matéria: {resultado["ID_Materia"]}, Nome da Matéria: {resultado["Nome_Materia"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}, Nota: {resultado["Nota"]}')
+
+# 2- Histórico de disciplinas ministradas por qualquer professor, com semestre e ano
+def query2():
+    with driver.session() as session:
+        query2 = session.run(
+        """
+        MATCH (p:Professor)-[:POSSUI]->(hp:HistóricoProfessor)-[:REFERENCIA]->(m:Matéria)
+        RETURN p.Nome_Professor AS Nome_Professor,
+            m.Nome_Materia AS Nome_Materia,
+            m.ID_Materia AS ID_Materia,
+            hp.Semestre AS Semestre,
+            hp.Ano AS Ano
+        LIMIT 1
+        """
+        )
+        for resultado in query2:
+            print(f'Nome do Professor: {resultado["Nome_Professor"]}, Nome da Matéria: {resultado["Nome_Materia"]}, ID da Matéria: {resultado["ID_Materia"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}')
+
+# 3- Listar alunos que já se formaram (foram aprovados em todos os cursos de uma matriz curricular) em um determinado semestre de um ano
+def query3():
+    with driver.session() as session:
+        numero_aleatorio = random.randint(0, 7) # Pega um número aleatório para buscar na lista Semestre
+        semestre_aleatorio = semestre[numero_aleatorio] # Para pegar essa variável dentro da Query é preciso colocar o '$' antes
+        query3 = session.run(
+        """
+        MATCH (a:Aluno)-[:TEM]->(he:HistóricoEscolar), (a:Aluno)-[:CURSOU]->(c:Curso)
+        WHERE he.Nota > 5 AND he.Semestre = $semestre_aleatorio AND he.Ano = 2020
+        RETURN a.Nome_Aluno AS Nome_Aluno,
+            c.Nome_Curso AS Nome_Curso,
+            c.ID_Curso AS ID_Curso,
+            he.Semestre AS Semestre,
+            he.Ano AS Ano,
+            he.Nota AS Nota
+        """, 
+        semestre_aleatorio = semestre_aleatorio # E agora eu digo para a Query qual o parâmetro que eu dei
+        )
+        for resultado in query3:
+            print(f'Nome do Aluno: {resultado["Nome_Aluno"]}, Nome do Curso: {resultado["Nome_Curso"]}, ID do Curso: {resultado["ID_Curso"]}, Semestre: {resultado["Semestre"]}, Ano: {resultado["Ano"]}, Nota: {resultado["Nota"]}')
+
+# 4- Listar todos os professores que são chefes de departamento, junto com o nome do departamento
+def query4():
+    with driver.session() as session:
+        query4 = session.run(
+            """
+            MATCH (d:Departamento)-[:ORGANIZA]->(p:Professor)
+            WHERE d.Chefe_Departamento = p.Nome_Professor
+            RETURN d.Nome_Departamento AS Nome_Departamento,
+                p.Nome_Professor AS Nome_Professor
+            """
+        )
+        for resultado in query4:
+            print(f'Nome do Departamento: {resultado["Nome_Departamento"]}, Nome do Professor: {resultado["Nome_Professor"]}')
 
 # 5- Saber quais alunos formaram um grupo de TCC e qual professor foi o orientador
+def query5():
+    with driver.session() as session:
+        query5 = session.run(
+            """
+            MATCH (a:Aluno)-[:FAZ]->(t:TCC)<-[:ORIENTADO]-(p:Professor)
+            RETURN a.Nome_Aluno AS Nome_Aluno,
+                p.Nome_Professor AS Nome_Professor,
+                t.ID_TCC AS ID_TCC
+            """
+        )
+        for resultado in query5:
+            print(f'Nome do Aluno: {resultado["Nome_Aluno"]}, Nome do Professor: {resultado["Nome_Professor"]}, ID do TCC: {resultado["ID_TCC"]}')
+
+
+# ----- CHAMANDO TODAS AS FUNÇÕES -----
+
+# Chamando todas as funções que cria cada Grafo
+try:
+    removeNos()
+    criaAluno()
+    criaHistoricoEscolar()
+    criaMateria()
+    criaProfessor()
+    adicionaProfessor()
+    criaHistoricoProfessor()
+    criaCurso()
+    criaDepartamento()
+    adicionaDepartamento()
+    criaTCC()
+    print("----- Grafos criados -----")
+finally:
+    driver.close()
+
+# Chamando todas as funções que criam as relações entre os Grafos
+try:
+    relacao_Aluno_HistoricoEscolar()
+    relacao_HistoricoEscolar_Materia()
+    relacao_Professor_HistoricoProfessor()
+    relacao_HistoricoProfessor_Materias()
+    relacao_Aluno_Curso()
+    relacao_Departamento_Professor()
+    relacao_Professor_TCC()
+    relacao_Aluno_TCC()
+    print("\n----- Relação dos Grafos criadas -----")
+finally:
+    driver.close()
+
+# Chamando todas as funções que criam as Queries
+try:
+    print('\n----- RESOLUÇÃO DAS QUERIES -----\n')
+    print("----- QUERY 1 -----")
+    query1()
+    print("\n----- QUERY 2 -----")
+    query2()
+    print("\n----- QUERY 3 -----")
+    query3()
+    print("\n----- QUERY 4 -----")
+    query4()
+    print("\n----- QUERY 5 -----")
+    query5()
+    print("\n ----- Programa finalizado -----")
+finally:
+    driver.close()
